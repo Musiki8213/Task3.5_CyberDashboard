e6f93e6391c2f7788539355c33afb71967d964d8
Musiki8213 <sithomolamusiki@gmail.com>
2025-07-17 22:28:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/*const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yArray = [55, 49, 44, 24, 15];
+
+const data = [{
+  x:xArray,
+  y:yArray,
+  type:"bar",
+  orientation:"v",
+  marker: {color:"rgba(0, 0, 0, 1)"}
+}];
+
+const layout = {title:"World Wide Wine Production"};
+
+Plotly.newPlot("myPlot", data, layout);*/
