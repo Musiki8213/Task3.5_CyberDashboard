ec329f2afab48766fc085def27396b24fc79748a
Musiki8213 <sithomolamusiki@gmail.com>
2025-07-20 21:53:45 +0200

    edited javascript and html

--- script.js
+++ script.js
@@ -119,3 +119,136 @@ const ctx = document.getElementById('card1Metric').getContext('2d');
 }
 );
     
+
+
+
+  const ctx4 = document.getElementById('card4Metric').getContext('2d')
+ const chart4 = new Chart(ctx4,{
+  type: "doughnut",
+  data: {
+    labels: ["Secure", "At Risk"],
+    datasets: [{
+      data: [85, 15],
+      backgroundColor: ["rgba(51, 98, 228, 1)", "#b9ae8fff"]
+    }]
+  },
+  options: {
+    cutout: "75%",
+    plugins: {
+      legend: { position: "right" }
+    }
+  }
+});
+
+const ctx5 = document.getElementById('card5Metric').getContext('2d')
+ const chart5 = new Chart(ctx5,{
+type: "bar",
+  data: {
+    labels: ["Firewall", "VPN", "IDS", "Antivirus"],
+    datasets: [{
+      label: "Protection Score",
+      data: [90, 80, 60, 85],
+      backgroundColor: "rgba(51, 98, 228, 1)"
+    }]
+  },
+  options: {
+    scales:{
+      y: {
+        min: 0,
+          max: 100,
+        title: {
+          display: true,
+          text: 'Protection Score'
+        }
+      },
+      x: {
+        title: {
+          display: true,
+          text: 'Network Security Types'
+        }
+      }
+    }
+  }
+});
+
+
+
+
+
+
+
+
+const ctx6 = document.getElementById('card6Metric').getContext('2d')
+ const chart6 = new Chart(ctx6,{
+ type: "bar",
+  data: {
+    labels: ["Startup Repair", "Safe Mode", "System Recovery", "Boot Logs"],
+    datasets: [{
+      label: "Effectiveness (%)",
+      data: [80, 90, 60, 70],
+      backgroundColor:'rgba(51, 98, 228, 1)',
+        borderColor: '#d9caf5ff',
+       borderWidth: 3
+    }]
+  },
+  options: {
+    scales: {y: {
+        min: 0,
+          max: 100,
+        title: {
+          display: true,
+          text: 'Effectiveness'
+        }
+      },
+      x: {
+        title: {
+          display: true,
+          text: 'Failure Recovery Tools'
+        }
+      }
+    }
+  }
+});
+
+
+
+
+
+const ctx7 = document.getElementById('card7Metric').getContext('2d')
+ const chart7 = new Chart(ctx7,{
+    type: 'line',
+    data: {
+      labels: ['Gaming', 'Productivity', 'Lifestyle', 'Others'],
+      datasets: [{
+        label: 'App Usage (%)',
+        data: [40, 25, 20, 15],
+        borderColor: 'rgba(12, 11, 85, 1)',
+        backgroundColor: '#d9caf5ff',
+        tension: 0.5,
+        fill: false,
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+        }
+      },
+      scales: {
+        y: {
+          min: 0,
+          max: 100,
+          title: {
+            display: true,
+            text: 'Usage (%)'
+          }
+        },
+        x: {
+          title: {
+            display: true,
+            text: 'App Categories'
+          }
+        }
+      }
+    }
+  });
